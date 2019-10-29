40fcbab7120c349ef49aceac19aeba64312d6e92
Lidia Garrido <97lidialgm@gmail.com>
2019-10-29 13:09:02 +0100

    Diseño Formulario

---
+++ app/src/main/java/es/iesfranciscodelosrios/algarrido/wolfrol/interfaces/Formulariointerface.java
@@ -0,0 +1,11 @@
+package es.iesfranciscodelosrios.algarrido.wolfrol.interfaces;
+
+public interface Formulariointerface {
+    public interface View{
+        void volverListado();
+    }
+
+    public interface Presenter{
+        void botonVolver();
+    }
+}
